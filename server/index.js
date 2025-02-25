0b8bbc23720e6d8846d9dbfed345ef78fcd9ea48
Anshu Patel <techanshupatel@gmail.com>
2025-02-26 01:37:31 +0530

    feat(server): set up Express server with CORS and environment variables

--- server/index.js
+++ server/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+
+// Load environment variables
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+// Load express and start the server
+const express = require('express');
+const app = express();
+
+// Load CORS
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
+
+
